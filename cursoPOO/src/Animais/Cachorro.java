0f171f9b5e8a9711fa3ffc6ddb45f424a8ec33d3
iamandaleao <amandabibiano144@gmail.com>
2025-10-01 18:01:45 -0300

    feat: criar classe Cachorro com atributos e métodos básicos

--- cursoPOO/src/Animais/Cachorro.java
+++ cursoPOO/src/Animais/Cachorro.java
@@ -5,7 +5,27 @@ public class Cachorro {
 
     public String nome;
     public String cor;
+    public String estadoDeEspirito;
     public int altura;
     public double peso;
     public int tamanhoDoRabo;
+
+    public void comer(){}
+
+    public void latir(){
+        System.out.println("AU AU");
+    }
+    public String pegar(){
+        return "Bolinha";
+    }
+    public String interagir(String acao){
+        if (acao.equals("carinho")){
+            this.estadoDeEspirito = "feliz";
+        }else if (acao.equals("Vai dormir!")){
+            this.estadoDeEspirito = "Bravo";
+        }else {
+            this.estadoDeEspirito = "Neutro";
+        }
+        return estadoDeEspirito;
+    }
 }
