011e4939f795cd235f40956b3f2e8af76cd706df
iamandaleao <amandabibiano144@gmail.com>
2025-11-04 14:28:51 -0300

    feat: add exercises

---
+++ java8-para21/src/ExemploVirtualThreads.java
@@ -0,0 +1,20 @@
+public class ExemploVirtualThreads {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100; i++) {
+            Thread.ofVirtual().start(() ->{
+                try {
+                    System.out.println("Executando Thread: " + Thread.currentThread());
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+        }
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
